f45080c1e88dddd01a40007e66128eeb23f2f05f
Jeferson Rios <jefry1795@hotmail.com>
2025-12-01 17:43:58 -0500

    Update Alimento schema to include cantidad and fecha_ingreso

--- BACKEND/app/schemas/alimento.py
+++ BACKEND/app/schemas/alimento.py
@@ -1,13 +1,19 @@
-from pydantic import BaseModel, Field
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel
 
 class AlimentoBase(BaseModel):
-    nombre: str  
+    nombre: str
+    cantidad: int
+    fecha_ingreso: date
   
 class AlimentoCreate(AlimentoBase):
     pass
 
 class AlimentoUpdate(BaseModel):
-    nombre: str  
+    nombre: Optional[str] = None
+    cantidad: Optional[int] = None
+    fecha_ingreso: Optional[date] = None
     
 class AlimentoOut(AlimentoBase):
     id_alimento: int
