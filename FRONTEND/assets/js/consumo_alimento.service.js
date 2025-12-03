aafe3029574646ede1db55cabac412d83c49a8cb
Jeferson Rios <jefry1795@hotmail.com>
2025-12-03 10:11:01 -0500

    Add files via upload

---
+++ FRONTEND/assets/js/consumo_alimento.service.js
@@ -0,0 +1,62 @@
+import { request } from './api/apiClient.js';
+
+export const chickenService = {
+    /**
+     * Obtener todos los rescates
+     * @returns {Promise<object>}
+     */
+    getConsumo: (page = 1, page_size = 10) => {
+        const endpoint = `/consumo_gallinas/all-consumos-pag?page=${page}&page_size=${page_size}`;
+        return request(endpoint);
+    },
+    
+    /**
+     * Obtener un rescate por su ID
+     * @param {number} consumoId - El ID del rescate a buscar
+     * @returns {Promise<object>}
+     */
+    getConsumoById: (consumoId) => {
+        // CORRECCIÓN: Nombre de función y parámetro
+        const endpoint = `/consumo_gallinas/by-id/${consumoId}`;
+        return request(endpoint);
+    },
+
+    getConsumoByGalpon: (galponId, page = 1, page_size = 10) => {
+        // CORRECCIÓN: Nombre de función y parámetro
+        const endpoint = `/consumo_gallinas/by-galpon?page=${page}&page_size=${page_size}&id_galpon=${galponId}`;
+        return request(endpoint);
+    },
+
+    getConsumoByRangeDate: (date_start, date_end, page = 1, page_size = 10) => {
+        // CORRECCIÓN: Nombre de función y parámetro
+        const endpoint = `/consumo_gallinas/rango-fechas?fecha_inicio=${date_start}&fecha_fin${date_end}&page=${page}&page_size=${page_size}`;
+        return request(endpoint);
+    },
+
+    /**
+     * Actualizar un rescate
+     * @param {string | number} consumoId - El ID del rescate a actualizar
+     * @param {object} consumoData - Los nuevos datos del rescate
+     * @returns {Promise<object>}
+     */
+    updateConsumo: (consumoId, consumoData) => {
+        // CORRECCIÓN: Nombre de función
+        return request(`/consumo_gallinas/by-id/${consumoId}`, {
+            method: 'PUT',
+            body: JSON.stringify(consumoData),
+        });
+    },
+
+    /**
+     * @param {object} consumoData - Los datos del nuevo rescate
+     * @returns {Promise<object>}
+     */
+    createConsumo: (consumoData) => {
+        // CORRECCIÓN: Nombre de función
+        return request(`/consumo_gallinas/crear`, {
+            method: 'POST',
+            body: JSON.stringify(consumoData),
+        });
+    },
+
+};
