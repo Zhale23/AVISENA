26d6063bd204dfd097c7d5e18b79f18a4b56373b
Jeferson Rios <jefry1795@hotmail.com>
2025-12-09 07:29:26 -0500

    Implement role status validation in auth.py
    Added role status check during user authentication.

--- BACKEND/app/router/auth.py
+++ BACKEND/app/router/auth.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends,HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 from app.router.dependencies import authenticate_user
 from app.schemas.auth import ResponseLoggin
 from core.security import create_access_token
@@ -19,14 +20,34 @@ async def login_for_access_token(
     if not user:
         raise HTTPException(
             status_code=401,
-            detail="Datos Incorrectos en email o password",
+            detail="Datos incorrectos en email o password.",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    
+    if not user.estado:
+        raise HTTPException(status_code=403, detail="Usuario inactivo, no autorizado")
+
+    # consultar estado del rol (activo o inactivo)
+    sentencia = text("""
+                    SELECT estado
+                    FROM roles
+                    WHERE id_rol = :rol 
+                    """)
+    res = db.execute(sentencia, {"rol": user.id_rol}).mappings().first()
+    
+    if not res:
+        raise HTTPException(status_code=404, detail="Rol no encontrado")
+    
+    # Si el estado del rol es inactivo
+    if res['estado'] == 0:
+        raise HTTPException(status_code=401, detail="Rol inactivo")
+
+
     data={"sub": str(user.id_usuario), "rol":user.id_rol}
     access_token = create_access_token(data)   
 
     return ResponseLoggin(
+
         user=user,
         access_token=access_token
     )
-
