d541432a6529e817aae42b705e000f727f4aab96
Luisa-039 <168785270+Luisa-039@users.noreply.github.com>
2025-12-09 19:15:10 -0500

    Update auth.py

--- BACKEND/app/schemas/auth.py
+++ BACKEND/app/schemas/auth.py
@@ -5,3 +5,10 @@ from app.schemas.users import UserOut
 class ResponseLoggin(BaseModel):
     user: UserOut
     access_token: str 
+
+class ForgotPasswordRequest(BaseModel):
+    email: EmailStr
+
+class ResetPasswordRequest(BaseModel):
+    token: str
+    new_password: str
