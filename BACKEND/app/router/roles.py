68bb3b5b7e117133ea3aee855395d553f922ea08
Mafe Garcia <garciacarvajalmariafernanda@gmail.com>
2025-12-14 22:57:23 -0500

    update roles.py

--- BACKEND/app/router/roles.py
+++ BACKEND/app/router/roles.py
@@ -117,7 +117,7 @@ def update_rol_by_id(
         raise HTTPException(status_code=500, detail=str(e))
     
     
-@router.put("/cambiar-estado/{user_id}", status_code=status.HTTP_200_OK)
+@router.put("/cambiar-estado/{rol_id}", status_code=status.HTTP_200_OK)
 def cambiar_rol_estado(
     rol_id: int,
     nuevo_estado: bool,
@@ -137,4 +137,3 @@ def cambiar_rol_estado(
     
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
