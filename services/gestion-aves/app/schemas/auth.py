5e360a77fc00f6a030826fba702858cc21e30ccd
Zharick Londoño <158623663+Zhale23@users.noreply.github.com>
2026-01-14 16:04:53 -0500

    Delete services directory

--- services/gestion-aves/app/schemas/auth.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-from app.schemas.users import UserOut
-
-class ResponseLoggin(BaseModel):
-    user: UserOut
-    access_token: str
--- services/seguridad-ventas/app/schemas/auth.py
+++
@@ -1,7 +0,0 @@
-from app.schemas.users import UserOut
-from pydantic import BaseModel, EmailStr, Field
-from typing import Optional
-
-class ResponseLoggin(BaseModel):
-    user: UserOut
-    access_token: str
